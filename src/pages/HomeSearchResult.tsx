e7b776bb3dbe1d7a1acad84e19d6a0ce17072b25
MohamedEssam07 <mohamed1220032003@gmail.com>
2025-11-06 22:31:28 +0200

    search for results updated

--- src/pages/HomeSearchResult.tsx
+++ src/pages/HomeSearchResult.tsx
@@ -1,7 +1,8 @@
 import { useParams } from "react-router";
 import ReusableGetHook from "../hooks/ReusableGetHook";
 import { useEffect, useState } from "react";
-import { Box, Grid, Text, useColorModeValue } from "@chakra-ui/react";
+import { Box, Flex, Grid, Text, useColorModeValue, HStack, Icon } from "@chakra-ui/react";
+import { SearchIcon } from "@chakra-ui/icons";
 import TvMovie from "../components/TvMovie";
 import type { IProduct } from "../Interfaces";
 import MovieCard from '../components/MoiveCard'
@@ -68,8 +69,55 @@ const HomeSearchResult = ({ }: IProps) => {
                 :
                 (
                     <>
+                        <Flex
+                            direction="column"
+                            align="center"
+                            justify="center"
+                            mt="140px"
+                            mb={8}
+                            px={4}
+                        >
+                            <HStack
+                                spacing={3}
+                                mb={2}
+                                bg={useColorModeValue("blue.50", "blue.900")}
+                                px={6}
+                                py={4}
+                                borderRadius="xl"
+                                boxShadow="0 4px 20px rgba(0, 0, 0, 0.1)"
+                                border="1px solid"
+                                borderColor={useColorModeValue("blue.200", "blue.700")}
+                            >
+                                <Icon as={SearchIcon} boxSize={6} color={useColorModeValue("blue.500", "blue.300")} />
+                                <Text
+                                    fontSize={{ base: "lg", md: "xl", lg: "2xl" }}
+                                    fontWeight="semibold"
+                                    color={useColorModeValue("gray.700", "gray.200")}
+                                >
+                                    Search Results for:
+                                </Text>
+                                <Text
+                                    fontSize={{ base: "lg", md: "xl", lg: "2xl" }}
+                                    fontWeight="bold"
+                                    bgGradient="linear(to-r, blue.400, blue.600)"
+                                    bgClip="text"
+                                    color="transparent"
+                                >
+                                    "{input}"
+                                </Text>
+                            </HStack>
+                            {data?.total_results && (
+                                <Text
+                                    fontSize="sm"
+                                    color={useColorModeValue("gray.600", "gray.400")}
+                                    mt={2}
+                                >
+                                    Found {data.total_results} result{data.total_results !== 1 ? 's' : ''}
+                                </Text>
+                            )}
+                        </Flex>
                         <Grid
-                            mt={"140px"}
+                            mt={8}
                             bg={bgCard}
 
                             border="1px solid"
